cc4bd31fcee27d9a64d7c5d8798bf1f2bb473ecb
jportilla192 <juanportilla192@gmail.com>
2024-01-20 09:20:39 -0500

    firts commit

---
+++ basic_calc.py
@@ -0,0 +1,20 @@
+'''
+ DESCRIPTION
+THIS CALCULATOR LET YOU GET RESULT OF 4 BASIC MATH OPERATIONS
+FROM TWO NUMBERS (VARIABLES).
+'''
+#INPUTS (TWO static VARIABLES)
+numb1 = 200
+numb2 = 12
+
+#PROCESS
+add = numb1 + numb2
+sub = numb1 - numb2
+mul = numb1 * numb2
+div = numb1 / numb2
+
+#OUTPUTS
+print("the adition is:", add)
+print("the substraction is:", sub)
+print("the multiplication is:", mul)
+print("the division is:", div)
